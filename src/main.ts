1e86dbd036da319bc6de4a124cbcbe641b092ae4
giuseppepetrazzuolo6 <giuseppepetrazzuolo6@gmail.com>
2026-03-03 12:48:00 +0100

    milestone-3

--- src/main.ts
+++ src/main.ts
@@ -44,3 +44,50 @@ type Actress = Person & {
     | "South Korean"
     | "Chinese";
 }
+
+/*📌 Milestone 3
+Crea una funzione getActress che, dato un id, effettua una chiamata a:
+
+GET /actresses/:id
+La funzione deve restituire l’oggetto Actress, se esiste, oppure null se non trovato.
+
+Utilizza un type guard chiamato isActress per assicurarti che la struttura del dato ricevuto sia corretta. */
+
+function isActress(data: any): data is Actress {
+  return (
+    typeof data === "object" &&
+    data !== null &&
+    typeof data.id === "number" &&
+    typeof data.name === "string" &&
+    typeof data.birth_year === "number" &&
+    (data.death_year === undefined || typeof data.death_year === "number") &&
+    typeof data.biography === "string" &&
+    typeof data.image === "string" &&
+    typeof data.awards === "string" &&
+    Array.isArray(data.most_famous_movies) &&
+    data.most_famous_movies.length === 3 &&
+    data.most_famous_movies.every((m: any) => typeof m === "string") &&
+    ["American","British","Australian","Israeli-American","South African","French","Indian","Israeli","Spanish","South Korean","Chinese"]
+      .includes(data.nationality)
+  );
+}
+
+async function getActress(id: number): Promise<Actress | null> {
+  try {
+    const response = await fetch(`http://localhost:3333/actresses/${id}`);
+
+ if (!response.ok) {
+      throw new Error(`Errore http ${response.status}: ${response.statusText}`);
+    }
+
+    const data = await response.json();
+
+    if (isActress(data)) {
+      return data;
+    }
+
+    return null;
+  } catch {
+    return null;
+  }
+}
